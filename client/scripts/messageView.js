f3c7b4247de471acc02a88506acb87cb30895e26
Jorge Castello <jorgio.castello@gmail.com>
2020-04-03 17:37:38 -0700

    Start work on creating a template

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,18 @@
 var MessageView = {
 
-  render: _.template(`
-      <!--
-      <div class="chat">
-        <div class="username"></div>
-        <div></div>
-      </div>
-      -->
-    `)
+  //Check if all the data exists - for example with username, if it doesn't exist
+  //We want to include anonymous
+  //If roomname doesn't exist, we want to call it like "Main Room"
 
+  render: _.template(`
+  <div class="chat">
+    <div class="username">
+
+      <%= user %>
+    </div>
+
+    <div></div>
+  </div>
+        `)
 };
+
